c698aedc6a4dd2b065ae72d166c48f9fec66f239
Bizthebee5406 <ejschell2014@gmail.com>
2026-03-25 11:19:08 -0400

    Updated age checks to moons in ClanTracker commands

---
+++ path/to/clan_commands.js
@@ -0,0 +1,14 @@
+// Assuming the relevant file is named 'clan_commands.js' and these lines are in that context:
+
+// Line 1106
+if (char["moons"] >= apprenticeMilestone) {
+    // Logic for apprentice milestone check
+}
+
+// Other parts of the code that might include checking age for warriors/milestone that needs to be replaced
+
+// Line 1111
+if (char["moons"] >= warriorMilestone) {
+    // Logic for warrior milestone check
+}
+
